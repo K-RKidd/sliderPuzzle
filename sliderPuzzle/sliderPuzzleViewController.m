6ee20c28f975d4a0b520e042523012ba23280c04
K-RKidd <kr248196@muhlenberg.edu>
2014-04-30 09:53:24 -0400

    update

--- sliderPuzzle/sliderPuzzleViewController.m
+++ sliderPuzzle/sliderPuzzleViewController.m
@@ -31,9 +31,6 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)shuffleTiles:(id)sender {
-    //setup so when pushed the tiles move to new positions
-}
 
 - (IBAction)slide:(id)sender {
     BOOL left = TRUE;
@@ -41,6 +38,23 @@
     BOOL up = TRUE;
     BOOL down = TRUE;
     UIButton *pressedButton = sender;
+    
+    //////// stay in bounds for x
+    if (pressedButton.frame.origin.x <= 38){
+        left = FALSE;
+    }
+    else if (pressedButton.frame.origin.x  >= 194){
+        right = FALSE;
+    }
+    //// stay in bounds for y
+    if (pressedButton.frame.origin.y  == 128){
+        up = FALSE;
+        
+    }
+    else if (pressedButton.frame.origin.y  == 284){
+        down = FALSE;
+    }
+    
     for (UIButton *b in tiles){
         ///// right
         if (pressedButton.frame.origin.x + 52 == b.frame.origin.x && pressedButton.frame.origin.y == b.frame.origin.y){
@@ -52,31 +66,18 @@
             NSLog(@ "can't move left");
             left = FALSE;
         }
-        /// up
+        /// down
         else if (pressedButton.frame.origin.y + 52 == b.frame.origin.y && pressedButton.frame.origin.x == b.frame.origin.x){
             NSLog(@ "can't move down");
-            up = FALSE;
+            down = FALSE;
         }
-        /// down
+        /// up
         else if (pressedButton.frame.origin.y - 52 == b.frame.origin.y && pressedButton.frame.origin.x == b.frame.origin.x){
             NSLog(@ "can't move up");
-            down = FALSE;
-        }
-        //////// stay in bounds
-        else if (pressedButton.frame.origin.x  <= 90){
-            left = FALSE;
-        }
-        else if (pressedButton.frame.origin.x  >= 241){
-            right = FALSE;
-        }
-        else if (pressedButton.frame.origin.y  <= 180){
             up = FALSE;
-            
-        }
-        else if (pressedButton.frame.origin.y   >= 330){
-            down = FALSE;
         }
     }
+   
     if (right){
         
         CGRect newFrame = CGRectMake(pressedButton.frame.origin.x + 52, pressedButton.frame.origin.y, 52, 52);
@@ -88,13 +89,13 @@
         [UIView animateWithDuration:1.0 animations:^{ pressedButton.frame = newFrame;}];
         
     }
-    else if (up){
+    else if (down){
         CGRect newFrame = CGRectMake(pressedButton.frame.origin.x, pressedButton.frame.origin.y + 52, 52, 52);
         [UIView animateWithDuration:1.0 animations:^{ pressedButton.frame = newFrame;}];
         
     }
 
-    else if (down){
+    else if (up){
         
         CGRect newFrame = CGRectMake(pressedButton.frame.origin.x, pressedButton.frame.origin.y - 52, 52, 52);
         [UIView animateWithDuration:1.0 animations:^{ pressedButton.frame = newFrame;}];
